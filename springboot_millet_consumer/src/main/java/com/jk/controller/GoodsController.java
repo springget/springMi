a70f4ffe40cf98c737c67f0e23f6616acff05648
whkokon <whkokwhkokon@163.com>
2019-08-20 21:26:49 +0800

    第一次上传

--- springboot_millet_consumer/src/main/java/com/jk/controller/GoodsController.java
+++ springboot_millet_consumer/src/main/java/com/jk/controller/GoodsController.java
@@ -2,6 +2,7 @@ package com.jk.controller;
 
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.jk.model.Allocation;
 import com.jk.model.Goods;
 import com.jk.model.Tree;
 import com.jk.model.Type;
@@ -16,9 +17,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import com.jk.util.OSSClientUtil;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Controller
@@ -69,6 +77,14 @@ public class GoodsController {
 
         return list;
     }
+    @RequestMapping("TypeOne")
+    @ResponseBody
+    public List<Allocation> TypeOne(){
+
+        List<Allocation> list = goodsService.TypeOne();
+
+        return list;
+    }
     //批量下架
     @RequestMapping("soldOut")
     @ResponseBody
@@ -114,5 +130,58 @@ public class GoodsController {
         for (String id:split) {
             goodsService.deleByIdTwo(id);
         }
+    }
+    @RequestMapping("uploadPhotoFile")
+    @ResponseBody
+    public String upImg(MultipartFile artImg, HttpServletRequest req) throws
+            Exception{
+    //获取原文件名称
+        String fileName = artImg.getOriginalFilename();
+        String folderPath =
+                req.getSession().getServletContext().getRealPath("/")+
+                        "upload/";
+        File file = new File(folderPath);
+    // 该目录是否已经存在
+        if(!file.exists()){
+    // 创建文件夹
+            file.mkdir();
+        }
+        String onlyFileName = UUID.randomUUID().toString()+fileName.substring(fileName.lastIndexOf('.'));
+        FileOutputStream fos = new FileOutputStream(folderPath+onlyFileName);
+        fos.write(artImg.getBytes());
+        fos.flush();
+        fos.close();
+        return "/upload/"+onlyFileName;
+    }
+    @RequestMapping("putawayOne")
+    @ResponseBody
+    public void putawayOne(Integer id){
+        goodsService.putawayOne(id);
+    }
+    @RequestMapping("ishot")
+    @ResponseBody
+    public void ishot(Integer id){
+        goodsService.ishot(id);
+    }
+    @RequestMapping("nohot")
+    @ResponseBody
+    public void nohot(String ids) {
+        String[] split = ids.split(",");
+        for (String id:split) {
+            goodsService.nohot(id);
+        }
+    }
+    @RequestMapping("updaloadImg")
+    @ResponseBody
+    public String uploadImg(MultipartFile imgg)throws IOException {
+        if (imgg == null || imgg.getSize() <= 0) {
+            throw new IOException("file不能为空");
+        }
+        OSSClientUtil ossClient=new OSSClientUtil();
+        String name = ossClient.uploadImg2Oss(imgg);
+        String imgUrl = ossClient.getImgUrl(name);
+        String[] split = imgUrl.split("\\?");
+        //System.out.println(split[0]);
+        return split[0];
     }
 }
